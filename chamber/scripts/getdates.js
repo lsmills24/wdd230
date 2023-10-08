455e455aab11cc7ad80af18c06747362c49ca39b
lsmills24 <97650683+lsmills24@users.noreply.github.com>
2023-10-07 23:19:03 -0600

    title and h1

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -4,3 +4,11 @@ document.getElementById('year').innerHTML = new Date().getFullYear();
 // Return date last modified
 let modified = document.lastModified;
 document.getElementById('sudo').innerHTML = modified;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+navigation.classList.toggle('open');
+hamButton.classList.toggle('open');
+});
