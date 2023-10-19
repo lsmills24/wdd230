fd4f078e9ce41dc1d13fb1f499a1126e97b84ec3
lsmills24 <97650683+lsmills24@users.noreply.github.com>
2023-10-18 23:44:46 -0600

    Fix full chamber dark mode, footer

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -13,24 +13,3 @@ hamButton.addEventListener('click', () => {
 navigation.classList.toggle('open');
 hamButton.classList.toggle('open');
 });
-
-
-// Dark Mode Slider
-const modeButton = document.querySelector("#mode");
-const main = document.querySelector("main");
-
-modeButton.addEventListener("click", () => {
-	if (modeButton.textContent.includes("◾")) {
-        document.body.style.background = "#000";
-        main.style.background = "#000";
-		main.style.color = "#fff";
-        // add rest of style changes here for dark mode
-		modeButton.textContent = "🔲";
-	} else {
-        document.body.style.background = "#F2FAFF";
-		main.style.background = "#F2FAFF";
-		main.style.color = "#000";
-        // add rest of style changes here for dark mode
-		modeButton.textContent = "◾";
-	}
-});
