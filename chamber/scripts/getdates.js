139a81f937b05ac2e07f0ee6e7b83dbfb57695a2
lsmills24 <97650683+lsmills24@users.noreply.github.com>
2023-10-28 21:35:36 -0600

    Discover chamber page

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -13,3 +13,40 @@ hamButton.addEventListener('click', () => {
 navigation.classList.toggle('open');
 hamButton.classList.toggle('open');
 });
+
+
+
+
+
+// Create the display variable
+const visitsDisplay = document.querySelector(".visits");
+
+// Get stored value in localstorage, if missing then assign 0 to it
+let numVisits = Number(window.localStorage.getItem("numVisits-1s")) || 0;
+
+// determine if this is the first visit or not
+if (numVisits !== 0) {
+    visitsDisplay.textContent = numVisits;
+} else {
+    visitsDisplay.textContent = 'This is your first visit. 👋 Welcome!';
+}
+
+// increase # of visits by one 
+numVisits++;
+
+// Store new visit # into local storage key=numVisits-1s
+localStorage.setItem("numVisits-1s", numVisits);
+
+
+
+
+
+const msToDays = 84600000;
+// today's date
+const theDateToday = new Date();
+
+// initialize display elements
+const todayElement = document.querySelector("#today");
+
+// processing
+const today = Date.now();
