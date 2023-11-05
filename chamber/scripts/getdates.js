d8a70487cc38a7d2873498df5dfb3062d7622c16
lsmills24 <97650683+lsmills24@users.noreply.github.com>
2023-11-05 16:49:55 -0700

    chamber discover visit messages via localStorage

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -14,39 +14,50 @@ navigation.classList.toggle('open');
 hamButton.classList.toggle('open');
 });
 
-
+//*************** Display number of page visits ************************************** */
 // Create the display variable
-const visitsDisplay = document.querySelector(".visits");
-
+// const visitsDisplay = document.querySelector(".visits");
 // Get stored value in localstorage, if missing then assign 0 to it
-let numVisits = Number(window.localStorage.getItem("numVisits-1s")) || 0;
-
+// let numVisits = Number(window.localStorage.getItem("numVisits-1s")) || 0;
 // determine if this is the first visit or not
-if (numVisits !== 0) {
-    visitsDisplay.textContent = numVisits;
-} else {
-    visitsDisplay.textContent = 'This is your first visit. 👋 Welcome!';
-}
-
+// if (numVisits !== 0) {
+//     visitsDisplay.textContent = numVisits;
+// } else {
+//     visitsDisplay.textContent = 'This is your first visit. 👋 Welcome!';
+// }
 // increase # of visits by one 
-numVisits++;
-
+// numVisits++;
 // Store new visit # into local storage key=numVisits-1s
-localStorage.setItem("numVisits-1s", numVisits);
+// localStorage.setItem("numVisits-1s", numVisits);
 
 
+//********************* Display Visit Messages ************************************** */
+const message = document.querySelector('.msg');  // references msg in discover.html to access message
 const msToDays = 84600000;
-
-// initialize display elements
-const todayElement = document.querySelector("#today");
-// today's date
-const today = Date.now();
-
-// references msg in discover.html to access message
-const message = document.querySelector('.msg');
+// const todayElement = document.querySelector("#today");
+const today = new Date();
 
 //localStorage
 let visitDate = new Date(window.localStorage.getItem('dateVisited-1s'));
+let daysSinceVisit = (today - visitDate) / msToDays;
+daysSinceVisit = daysSinceVisit.toFixed(0);  //rounds the milliseconds to whole number
+
+console.log(daysSinceVisit)
+
+if (visitDate.getFullYear() < 1970) {
+    message.textContent = "Welcome! Let us know if you have any questions.";
+} else if (daysSinceVisit < 1) {
+    message.textContent = "Back so soon? Awesome!";
+} else {
+    if (daysSinceVisit == 1) {
+        message.textContent = "You visited this site ${daysSinceVisit} day ago.";
+    }
+    if (daysSinceVisit > 1) {
+        message.textContent = "You visited this site ${daysSinceVisit} days ago.";
+    }
+}
+// Store new visit total into localStorage (key=numVisits-1s)
+localStorage.setItem("dateVisited-1s", today);
 
 
 //************************ JOIN FORM TIMESTAMP *******************************/
@@ -54,7 +65,7 @@ let visitDate = new Date(window.localStorage.getItem('dateVisited-1s'));
 function getTimestamp() {
     const now = new Date();
     return now.toISOString();
-}
+};
 
 // Event listener to populate the timestamp input field when the form is submitted
 document.getElementById("joinForm").addEventListener("submit", function(event) {
