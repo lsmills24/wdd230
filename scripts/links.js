672d9690e4116f93b299f33016c92bd33c6f284f
lsmills24 <97650683+lsmills24@users.noreply.github.com>
2023-11-10 21:26:09 -0700

    links

--- scripts/links.js
+++ scripts/links.js
@@ -14,43 +14,26 @@ async function getlinkdata() {
 
 getlinkdata();
 
-// Not working - Ignore but don't delete yet! **************************************
-const displayLinks_____ = (weeks) => {  
-    weeks.forEach((week) => {
-        let section = document.createElement('h4');
-        let link = document.createElement('a')
-        let list = document.createElement('li');
-        let title = document.createElement('p');
-
-        section.textContent = `${week.lesson}`;
-        link.textContent = `${week.links.url}`;
-        // list.setAttribute('href', week.links.url);
-        // list.setAttribute('class', 'lesson');
-        // list.textContent = title;
-        section.appendChild(link);
-        section.appendChild(title);
-
-        cards.appendChild(section);
-    });
-}
-
-// Attempting this one ************************************************************
+// Currently working function with nested for loop inside forEach loop
 const displayLinks = (weeks) => {
+    const list = document.getElementById("list");
+ 
     weeks.forEach((week) => {
-        let card = document.createElement('li');
-        let link = document.createElement('a');
-        let assignmentName = document.createElement('p');
-
-        // ******** Try Nested forEach function for each assignment in each lesson ********* //
-        card.textContent = `Week ${week.lesson}: `;
-
-        link.setAttribute('class', 'lesson');
-        link.setAttribute('href', week.links.url);
-        link.textContent = `${week.links.title} |`;
-
-        card.appendChild(link);
-        // card.appendChild();
-
-        cards.appendChild(card);
+        let listItem = document.createElement('li');
+        listItem.textContent = `Week ${week.lesson}: `;
+        for (let i = 0; i < week.links.length; i++) {
+            // console.log(week.links[i].title);
+            // console.log(week.links[i].url);
+            let alink = document.createElement('a');
+            alink.setAttribute('href', week.links[i].url);
+            alink.setAttribute('class', 'lesson');
+            if (i == 0) {
+                alink.text = week.links[i].title;
+            } else {
+                alink.textContent = ` | ${week.links[i].title}`;
+            }
+            listItem.appendChild(alink);
+        }
+        list.appendChild(listItem);
     });
 }
